14f44657d6f38dc3bdb5ed3b8a77b1bec6084926
marialuisacampos <luisa.campos@ufrpe.br>
2022-06-23 20:12:32 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Lesson from "./components/Lesson"
+import Event from "./pages/Event"
+
+function App() {
+
+  return (
+    <Event />
+  )
+}
+
+export default App
